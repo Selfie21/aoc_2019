5ba21af43931cf0389de0c30486f36a8d98ae2b0
Pierre Brosemer <pierre.brosemer@gmail.com>
2020-01-21 12:59:43 +0100

    old solution was horrible. Felt like cheating using the networkx package but dont have much time today so... Still looks pretty clean though!

--- codeofadvent/sixth.py
+++ codeofadvent/sixth.py
@@ -1,40 +1,32 @@
+import networkx as nx
+
+
 def read_linestxt(file):
     return file.read().splitlines()
 
 
+def print_solution(first, second):
+    print("First: " + str(first) + ", Second " + str(second))
+
+
+def create_graph(file):
+    g = nx.Graph()
+    for line in read_linestxt(file):
+        u = line.split(')')[0]
+        v = line.split(')')[1]
+        g.add_node(u)
+        g.add_node(v)
+        g.add_edge(u, v)
+    return g
+
+
 f = open("sixth.txt", "r")
+graph = create_graph(f)
+first = 0
 
+for node in graph.nodes:
+    first += nx.shortest_path_length(graph, node, "COM")
 
-first = []
-second = []
-for line in read_linestxt(f):
-    first.append(line.split(')')[0])
-    second.append(line.split(')')[1])
+second = nx.shortest_path_length(graph, "YOU", "SAN") - 2
 
-
-queue = []
-counter = 0
-increment = 0
-next_layer = ""
-queue.append("COM")
-new_layer = True
-
-while len(queue) > 0:
-    indices = [i for i, x in enumerate(first) if x == queue[0]]
-
-    for _ in indices:
-        queue.append(second[_])
-        counter += increment
-        if new_layer:
-            next_layer = second[_]
-
-    new_layer = False
-
-
-    if next_layer == queue[0]:
-        increment += 1
-        new_layer = True
-    queue.pop(0)
-
-    print(counter)
-
+print_solution(first, second)
