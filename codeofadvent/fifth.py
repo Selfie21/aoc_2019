982a9f0717fc03e37b2fffbdfb9a2fda7733d2bc
Pierre Brosemer <pierre.brosemer@gmail.com>
2020-01-23 19:15:48 +0100

    pretty happy with both solutions. I did this from scratch so no refactoring. I really like Intcode!

--- codeofadvent/fifth.py
+++ codeofadvent/fifth.py
@@ -11,65 +11,64 @@ def print_solution(first, second):
 
 class Intcomputer:
 
-    def __init__(self):
-        pass
+    def __init__(self, counter):
+        self.counter = counter
 
-    def get_setting(self, inputs, counter):
+    def get_setting(self, inputs):
         modes = ''
-        opcode = str(inputs[counter])[-1:]
-        if len(str(inputs[counter])) > 2:
-            modes = str(inputs[counter])[0:-2]
+        opcode = str(inputs[self.counter])[-1:]
+        if len(str(inputs[self.counter])) > 2:
+            modes = str(inputs[self.counter])[0:-2]
         return int(opcode), modes
 
-    def get_parameters(self, inputs, counter, modes):
-        params = [inputs[counter+1], inputs[counter+2], inputs[counter+3]]
+    def get_parameters(self, inputs, modes):
+        params = [inputs[self.counter+1], inputs[self.counter+2], inputs[self.counter+3]]
         for i in range(len(modes)):
             if modes[-(i+1)] == '1':
-                params[i] = counter + i + 1
+                params[i] = self.counter + i + 1
         return params
 
     def execute_intcode(self, inputs, enviroment, previous_output):
-        counter = opcode = 0
+        opcode = 0
         firstInput = True
 
-        while not(opcode == 9) and counter < len(inputs)-2:
-            opcode, modes = self.get_setting(inputs, counter)
-            params = self.get_parameters(inputs, counter, modes)
+        while not(opcode == 9) and self.counter < len(inputs)-2:
+            opcode, modes = self.get_setting(inputs)
+            params = self.get_parameters(inputs, modes)
 
             if opcode == 1:
-                inputs[inputs[counter + 3]] = inputs[params[0]] + inputs[params[1]]
-                counter += 4
+                inputs[inputs[self.counter + 3]] = inputs[params[0]] + inputs[params[1]]
+                self.counter += 4
 
             elif opcode == 2:
-                inputs[inputs[counter + 3]] = inputs[params[0]] * inputs[params[1]]
-                counter += 4
+                inputs[inputs[self.counter + 3]] = inputs[params[0]] * inputs[params[1]]
+                self.counter += 4
 
             elif opcode == 3:
                 inputs[params[0]] = enviroment if firstInput else previous_output
                 firstInput = False
-                counter += 2
+                self.counter += 2
 
             elif opcode == 4:
+                self.counter += 2
                 return inputs[params[0]]
 
             elif opcode == 5:
-                counter = inputs[params[1]] if inputs[params[0]] != 0 else counter + 3
+                self.counter = inputs[params[1]] if inputs[params[0]] != 0 else self.counter + 3
 
             elif opcode == 6:
-                counter = inputs[params[1]] if inputs[params[0]] == 0 else counter + 3
+                self.counter = inputs[params[1]] if inputs[params[0]] == 0 else self.counter + 3
 
             elif opcode == 7:
-                inputs[inputs[counter + 3]] = 1 if inputs[params[0]] < inputs[params[1]] else 0
-                counter += 4
+                inputs[inputs[self.counter + 3]] = 1 if inputs[params[0]] < inputs[params[1]] else 0
+                self.counter += 4
 
             elif opcode == 8:
-                inputs[inputs[counter + 3]] = 1 if inputs[params[0]] == inputs[params[1]] else 0
-                counter += 4
+                inputs[inputs[self.counter + 3]] = 1 if inputs[params[0]] == inputs[params[1]] else 0
+                self.counter += 4
 
-        return inputs[0]
+        return None
 
 
 f = open("fifth.txt", "r")
 inputs = read_commastxt(f.readline())
-computer = Intcomputer()
-computer.execute_intcode(inputs)
--- codeofadvent/seventh.py
+++ codeofadvent/seventh.py
@@ -1,6 +1,5 @@
 from fifth import Intcomputer
 
-
 def read_commastxt(line):
     inputs = []
     for number in line.split(','):
@@ -8,25 +7,87 @@ def read_commastxt(line):
     return inputs
 
 
+def print_solution(first, second):
+    print("First: " + str(first) + ", Second " + str(second))
+
+
 def test_amplifier(param, computer, inputs, previous):
     output = computer.execute_intcode(inputs, param, previous)
     return output
 
 
-def number_tolist(number, list):
-    number = str(number)
-    for i in range(len(list)):
-        list[i] = number[i]
+def get_permutation(current, mod):
+    contains_doubles = True
+    while contains_doubles:
+        for i in range(5):
+            if current[i] < mod:
+                current[i] += 1
+                break
+            else:
+                current[i] = 0
+        contains_doubles = check_doubles(current)
 
+
+def check_doubles(current):
+
+    for i in range(5):
+        instances = 0
+        tocheck = current[i]
+        for i in range(5):
+            if current[i] == tocheck:
+                instances += 1
+        if instances > 1:
+            return True
+    return False
 
 
 f = open("seventh.txt", "r")
 inputs_original = read_commastxt(f.readline())
-computer = Intcomputer()
+computer = Intcomputer(0)
 
+
+# First Exercise
 tunings = [0, 0, 0, 0, 0]
 previous = 0
+maximum = 0
 
-for _ in range(5):
-    inputs = inputs_original.copy()
-    previous = test_amplifier(tunings[_], computer, inputs, previous)
+for i in range(256):
+    previous = 0
+    for _ in range(5):
+        computer.counter = 0
+        inputs = inputs_original.copy()
+        previous = test_amplifier(tunings[_], computer, inputs, previous)
+    if maximum < previous:
+        maximum = previous
+    get_permutation(tunings, 4)
+
+
+# Second Exercise
+tunings = [0, 0, 0, 0, 0]
+previous = second = 0
+inputs = inputs_original.copy()
+last_halt = True
+tapes = []
+parameters = []
+
+
+for i in range(5):
+    tapes.append(inputs_original.copy())
+    parameters.append([tunings[i], tunings[i], Intcomputer(0)])
+parameters[0][1] = 0
+
+
+while last_halt:
+    for _ in range(5):
+        if not(previous is None):
+
+            previous = test_amplifier(parameters[_][0], parameters[_][2], tapes[_], parameters[_][1])
+            parameters[(_+1) % 5][0] = parameters[(_+1) % 5][1]
+            parameters[(_+1) % 5][1] = previous
+            if not(previous is None):
+                second = previous
+        else:
+            last_halt = False
+
+
+print_solution(maximum, second)
