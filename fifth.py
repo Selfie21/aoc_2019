aa7ee1dd37ff734a54978d3019215f6ab11c6072
Pierre Brosemer <pierre.brosemer@gmail.com>
2020-01-23 19:20:29 +0100

    cleaning up repo

--- fifth.py
+++ fifth.py
@@ -9,63 +9,66 @@ def print_solution(first, second):
     print("First: " + str(first) + ", Second " + str(second))
 
 
-def get_setting(inputs, counter):
-    modes = ''
-    opcode = str(inputs[counter])[-1:]
-    if len(str(inputs[counter])) > 2:
-        modes = str(inputs[counter])[0:-2]
-    return int(opcode), modes
+class Intcomputer:
 
+    def __init__(self, counter):
+        self.counter = counter
 
-def get_parameters(inputs, counter, modes):
-    params = [inputs[counter+1], inputs[counter+2], inputs[counter+3]]
-    for i in range(len(modes)):
-        if modes[-(i+1)] == '1':
-            params[i] = counter + i + 1
-    return params
+    def get_setting(self, inputs):
+        modes = ''
+        opcode = str(inputs[self.counter])[-1:]
+        if len(str(inputs[self.counter])) > 2:
+            modes = str(inputs[self.counter])[0:-2]
+        return int(opcode), modes
 
+    def get_parameters(self, inputs, modes):
+        params = [inputs[self.counter+1], inputs[self.counter+2], inputs[self.counter+3]]
+        for i in range(len(modes)):
+            if modes[-(i+1)] == '1':
+                params[i] = self.counter + i + 1
+        return params
 
-def execute_intcode(inputs):
-    counter = opcode = 0
+    def execute_intcode(self, inputs, enviroment, previous_output):
+        opcode = 0
+        firstInput = True
 
-    while not(opcode == 9) and counter < len(inputs)-2:
-        opcode, modes = get_setting(inputs, counter)
-        params = get_parameters(inputs, counter, modes)
+        while not(opcode == 9) and self.counter < len(inputs)-2:
+            opcode, modes = self.get_setting(inputs)
+            params = self.get_parameters(inputs, modes)
 
-        if opcode == 1:
-            inputs[inputs[counter + 3]] = inputs[params[0]] + inputs[params[1]]
-            counter += 4
+            if opcode == 1:
+                inputs[inputs[self.counter + 3]] = inputs[params[0]] + inputs[params[1]]
+                self.counter += 4
 
-        elif opcode == 2:
-            inputs[inputs[counter + 3]] = inputs[params[0]] * inputs[params[1]]
-            counter += 4
+            elif opcode == 2:
+                inputs[inputs[self.counter + 3]] = inputs[params[0]] * inputs[params[1]]
+                self.counter += 4
 
-        elif opcode == 3:
-            inputs[params[0]] = 5
-            counter += 2
+            elif opcode == 3:
+                inputs[params[0]] = enviroment if firstInput else previous_output
+                firstInput = False
+                self.counter += 2
 
-        elif opcode == 4:
-            print(inputs[params[0]])
-            counter += 2
+            elif opcode == 4:
+                self.counter += 2
+                return inputs[params[0]]
 
-        elif opcode == 5:
-            counter = inputs[params[1]] if inputs[params[0]] != 0 else counter + 3
+            elif opcode == 5:
+                self.counter = inputs[params[1]] if inputs[params[0]] != 0 else self.counter + 3
 
-        elif opcode == 6:
-            counter = inputs[params[1]] if inputs[params[0]] == 0 else counter + 3
+            elif opcode == 6:
+                self.counter = inputs[params[1]] if inputs[params[0]] == 0 else self.counter + 3
 
-        elif opcode == 7:
-            inputs[inputs[counter + 3]] = 1 if inputs[params[0]] < inputs[params[1]] else 0
-            counter += 4
+            elif opcode == 7:
+                inputs[inputs[self.counter + 3]] = 1 if inputs[params[0]] < inputs[params[1]] else 0
+                self.counter += 4
 
-        elif opcode == 8:
-            inputs[inputs[counter + 3]] = 1 if inputs[params[0]] == inputs[params[1]] else 0
-            counter += 4
+            elif opcode == 8:
+                inputs[inputs[self.counter + 3]] = 1 if inputs[params[0]] == inputs[params[1]] else 0
+                self.counter += 4
 
-    return inputs[0]
+        return None
 
 
 f = open("fifth.txt", "r")
 inputs = read_commastxt(f.readline())
-execute_intcode(inputs)
-
